0864a991d0886929d421f8fa04579e4d9051b9f6
gydnjsdl <khw33352182@gmail.com>
2025-06-11 10:43:26 +0900

    d

--- Script/MiddleWare/Logger.cs
+++ Script/MiddleWare/Logger.cs
@@ -6,12 +6,10 @@ public class Logger : ILogger
 {
     public void Log(string message)
     {
-        //todo : ���߿� FireBase �����̳� �񵿱� �Լ��� ���� �Ϸ���� �ɸ� �ð� Ȥ�� ����� GPU�Ǵ� CPU ��뷮�� �߰�.
         Debug.Log(message);
     }
     public void Error(string message)
     {
-        //todo : FireBase ���� ���̺�� ���� �޼��� �ֱ�
         Debug.Log(message);
     }
     
